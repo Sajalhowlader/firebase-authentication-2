41c8723ff356bf35703fbdfd3bc8a1f00a20c034
Sajalhowlader <sajolhowlader08@gmail.com>
2022-04-25 08:46:15 +0600

    addd pagination

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -7,15 +7,34 @@ import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
-    const [products, setProducts] = useProducts();
     const [cart, setCart] = useState([]);
+    const [pageCounts, setPageCounts] = useState(0);
+    const [pages, setPages] = useState(0)
+    const [size, setSize] = useState(10)
+    const [products, setProducts] = useState([]);
 
-    useEffect( () =>{
+    useEffect(() => {
+        fetch(`http://localhost:5000/products?pages=${pages}&size=${size}`)
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, [pages, size]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/productsCount')
+            .then(res => res.json())
+            .then(data => {
+                const count = data.count;
+                const pages = Math.ceil(count / 10)
+                setPageCounts(pages)
+            })
+    }, [])
+
+    useEffect(() => {
         const storedCart = getStoredCart();
         const savedCart = [];
-        for(const id in storedCart){
-            const addedProduct = products.find(product => product.id === id);
-            if(addedProduct){
+        for (const id in storedCart) {
+            const addedProduct = products.find(product => product._id === id);
+            if (addedProduct) {
                 const quantity = storedCart[id];
                 addedProduct.quantity = quantity;
                 savedCart.push(addedProduct);
@@ -24,35 +43,55 @@ const Shop = () => {
         setCart(savedCart);
     }, [products])
 
-    const handleAddToCart = (selectedProduct) =>{
+    const handleAddToCart = (selectedProduct) => {
         console.log(selectedProduct);
         let newCart = [];
-        const exists = cart.find(product => product.id === selectedProduct.id);
-        if(!exists){
+        const exists = cart.find(product => product._id === selectedProduct._id);
+        if (!exists) {
             selectedProduct.quantity = 1;
             newCart = [...cart, selectedProduct];
         }
-        else{
-            const rest = cart.filter(product => product.id !== selectedProduct.id);
+        else {
+            const rest = cart.filter(product => product._id !== selectedProduct._id);
             exists.quantity = exists.quantity + 1;
             newCart = [...rest, exists];
         }
-        
+
         setCart(newCart);
-        addToDb(selectedProduct.id);
+        addToDb(selectedProduct._id);
     }
 
     return (
         <div className='shop-container'>
             <div className="products-container">
                 {
-                    products.map(product=><Product 
-                        key={product.id}
+                    products.map(product => <Product
+                        key={product._id}
                         product={product}
                         handleAddToCart={handleAddToCart}
-                        ></Product>)
+                    ></Product>)
                 }
+                <div>
+                    {
+                        [...Array(pageCounts).keys()].map(number => <button
+                            className={pages === number ? 'selected' : ''}
+                            onClick={() => setPages(number)}>{number}</button>)
+
+                    }
+                    <select onChange={e => setSize(e.target.value)}>
+                        <select defaultValue={'DEFAULT'} >
+                            <option value="DEFAULT" disabled>Choose a salutation ...</option>
+                            <option value="5">5</option>
+                            <option value="10">10</option>
+                            <option value="15">15</option>
+                            <option value="20">20</option>
+                            <option value="25">25</option>
+                        </select>
+                    </select>
+
+                </div>
             </div>
+
             <div className="cart-container">
                 <Cart cart={cart}>
                     <Link to="/orders">
